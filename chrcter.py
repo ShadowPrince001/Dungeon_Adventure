b2f5eec39b3a7c3a9f6c7dbcc59746fd0ba94f7a
Prince <89309926+ShadowPrince001@users.noreply.github.com>
2022-09-30 13:09:28 +0530

    Update chrcter.py

--- chrcter.py
+++ chrcter.py
@@ -1,59 +1,94 @@
 import random
 
-stats={"Strength":0,
-       "Dexterity":0,
-       "Constitution":0,
-       "Intelligence":0,
-       "Wisdom":0,
-       "Charisma":0,
-       "Perception":0
-       }
+stats = {
+    "Strength": 0,
+    "Dexterity": 0,
+    "Constitution": 0,
+    "Intelligence": 0,
+    "Wisdom": 0,
+    "Charisma": 0,
+    "Perception": 0,
+}
 
-stats_list=['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma', 'Perception']
+stats_list = [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma",
+    "Perception",
+]
+
+
 def avatar():
-    name=str(input("Welcome to Dungeon Adventure. I am the Dungeon Master. Adventurer, What is your name?\n"))
-    print(name,",Lets start building your character")
+    name = str(
+        input(
+            "Welcome to Dungeon Adventure. I am the Dungeon Master. Adventurer, What is your name?\n"
+        )
+    )
+    print(name, ",Lets start building your character")
+    print(
+        "The attributes you can choose are ",
+        stats_list,
+    )
     for n in range(7):
-        rolls=[]
-        stat=0
+        rolls = []
+        stat = 0
         print("You rolled a ")
         for a in range(4):
-            roll=random.randint(1,6)
-            stat+=roll
+            roll = random.randint(1, 6)
+            stat += roll
             rolls.append(roll)
         print(rolls)
-        print("So, your final value is ",stat)
-        b=0
+        print("So, your final value is ", stat)
+        b = 0
         while b == 0:
             try:
-                x=str(input("What attribute will you like to assign your value of "+str(stat)+" to?\n"))
-                if stats[x] ==0:
-                    b +=1
+                x = str(
+                    input(
+                        "What attribute will you like to assign your value of "
+                        + str(stat)
+                        + " to?\n"
+                    )
+                )
+                if stats[x] == 0:
+                    b += 1
+                else:
+                    print(
+                        "The attributes you can choose are ",
+                        stats_list,
+                    )
+
             except KeyError:
-                print("That is not an attribute. The attributes you can choose are ",stats_list)
+                print(
+                    "That is not an attribute. The attributes you can choose are ",
+                    stats_list,
+                )
                 continue
-        stats[x]=stat
+        stats[x] = stat
         stats_list.remove(x)
         print("Your current stats are\n")
         for key, value in stats.items():
-            print(key, ' : ', value)
+            print(key, " : ", value)
     print("Your character is almost ready.")
-    health = 3*(int(stats["Constitution"])+random.randint(1,6))
-    print("Your starting health is",health)
+
+    health = 3 * (int(stats["Constitution"]) + 2 * (random.randint(1, 20)))
+    print("Your starting health is", health)
     print("Rolling for your starting gold. You rolled a ")
-    rolls=[]
-    gold=0
-    if stats["Charisma"]>=15:
-        x=5
+    rolls = []
+    gold = 0
+    if stats["Charisma"] >= 15:
+        x = 5
     else:
-        x=0
-    for n in range(20+x):
-        roll=random.randint(1,6)
+        x = 0
+    for n in range(25 + x):
+        roll = random.randint(1, 6)
         rolls.append(roll)
-        gold+=roll
+        gold += roll
     for i in rolls:
-        print(rolls[i], end = " ")
-    print("\nYour starting gold is",gold)
+        print(rolls[i], end=" ")
+    print("\nYour starting gold is", gold)
     return health, gold, stats
 
 
