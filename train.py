8968e01fb96613b1eabf50d67afd53a8a7bf9ee3
Prince <89309926+ShadowPrince001@users.noreply.github.com>
2022-10-09 13:13:04 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,29 @@
+stats_list = [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intellect",
+    "Luck",
+    "Charisma",
+    "Perception",
+]
+stat_pt = 0
+gld = 0
+
+
+def train(gold, trn):
+    while True:
+
+        stat = str(input(f"What attribute would you like to train?\n"))
+        if stat in stats_list:
+            gld = 50 * (1 + trn)
+            if gld > gold:
+                print("You do not have enough gold to train.")
+            else:
+                stat_pt = 1
+                return stat, gld, stat_pt
+        else:
+            print(
+                "That is not an attribute. The attributes you can choose are ",
+                stats_list,
+            )
